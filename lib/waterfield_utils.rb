0bbeec6c0a52aa6500d0e6166d46e9a884711025
Codie Mullins <codie.mullins@waterfield.com>
2016-12-08 12:39:57 -0600

    Remove sample code from tutorial

--- lib/waterfield_utils.rb
+++ lib/waterfield_utils.rb
@@ -1,6 +1,5 @@
 require 'waterfield_utils/acts_as_waterfield_utils'
 require 'waterfield_utils/auto_fixer'
-require 'waterfield_utils/core_ext'
 require 'waterfield_utils/permitted_params'
 require 'waterfield_utils/relationship'
 
--- spec/core_ext_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe 'to_squawk' do
-  it "should prepend the word squawk!" do
-    expect('Hello World'.to_squawk).to eq 'squawk! Hello World'
-  end
-end
